6a93120611311022c91b4d04133f91e0ca99153b
pi <mike.ackerschott03@gmail.com>
2023-05-23 06:26:06 -0400

    updated install.sh to take executing as sudo into account

--- RPI_master/install.sh
+++ RPI_master/install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+cd /home/$SUDO_USER/
 sudo apt-get update
 git clone https://github.com/MikeAckerschott/RFID_productherkenning.git
 sudo apt install make
 sudo apt install cmake
 sudo apt-get install build-essential
 sudo apt-get install libwxgtk3.0-gtk3-dev
-cd ~/
+cd /home/$SUDO_USER/
 wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.71.tar.gz
 tar zxvf bcm2835-1.71.tar.gz
 cd bcm2835-1.71
@@ -13,7 +14,7 @@ cd bcm2835-1.71
 make
 sudo make check
 sudo make install
-cd ~/RFID_productherkenning/
+cd /home/$SUDO_USER/RFID_productherkenning/
 mkdir ./RPI_master/build && cd ./RPI_master/build/
 cmake ../
 make -j4
